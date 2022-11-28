6a915fa02ab93311bb49d3a5fbeddeb26919e0d9
Vivien Song <118250919+viviensong@users.noreply.github.com>
2022-11-29 00:36:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,13 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
