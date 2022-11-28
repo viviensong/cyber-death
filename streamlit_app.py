4fb0ae2b5f8ed250c8f602da7138c4116f51665a
Vivien Song <118250919+viviensong@users.noreply.github.com>
2022-11-29 00:17:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import streamlit as st
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
 
 
 with st.echo(code_location='below'):
